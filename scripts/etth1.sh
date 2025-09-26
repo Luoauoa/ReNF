f5e1c518ddc633301558be61bbf6c9376685d732
Yihang LU <62492028+Luoauoa@users.noreply.github.com>
2025-09-26 10:22:26 +0800

    Update etth1.sh

--- scripts/etth1.sh
+++ scripts/etth1.sh
@@ -16,6 +16,34 @@ model_id_name=ETTh1
 data_name=ETTh1
 
 random_seed=2021
+# There are two versions of configurations (Hyperparameter searching) achieving similar results. The following is an example.
+for pred_len in 96
+do
+    python -u run_longExp.py \
+      --random_seed $random_seed \
+      --is_training 1 \
+      --root_path $root_path_name \
+      --data_path ${data_path_name} \
+      --model_id ${model_id_name}'_'$seq_len'_'$pred_len \
+      --model $model_name \
+      --data ${data_name} \
+      --features M \
+      --pe 1\
+      --revin 1\
+      --d_layers 3\
+      --level_dim 1\
+      --seq_len $seq_len \
+      --pred_len $pred_len \
+      --r_ema 0.995\
+      --alpha_freq 0.0\
+      --d_ff 256\
+      --gamma 0.9 \
+      --dropout 0.8\
+      --des 'Exp' \
+      --patience 3\
+      --train_epochs 10 \
+      --itr 1 --batch_size 64 --learning_rate 1e-3 >logs/LongForecasting/$model_name'_'$model_id_name'_'$seq_len'_'$pred_len.log 
+done
 
 for pred_len in 96
 do
